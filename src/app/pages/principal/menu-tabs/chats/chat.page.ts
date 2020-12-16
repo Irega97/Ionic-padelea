f177031106dd8c93d4d234491138f7a44c618870
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2020-12-16 19:40:43 +0100

    'ChatHTMLAvanzando'

--- src/app/pages/principal/menu-tabs/chats/chat.page.ts
+++ src/app/pages/principal/menu-tabs/chats/chat.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { ToastController } from '@ionic/angular';
 import { EventsService } from 'src/app/services/events.service';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -10,11 +11,45 @@ import { EventsService } from 'src/app/services/events.service';
 })
 export class ChatPage implements OnInit {
 
-  message = '';
-  messages= []; 
+  chats;
+  chatsSearch;
 
-  constructor( private eventService: EventsService ) { }
+  constructor( private events: EventsService, private chatService : ChatService ) { }
 
   ngOnInit() {
+    this.chatService.getMyChats().subscribe((data) => {
+      console.log(data);
+      this.chats = data;
+      this.chatsSearch = this.chats;      
+    }); 
+    this.events.getObservable().subscribe((data)=> {
+      if (data.topic == "new-chat") {
+        this.chatService.getMyChats().subscribe((data) => {
+          this.chats = data;
+          this.chatsSearch = this.chats;      
+        });
+      }
+    });
+    
   }
+
+  /* const searchbar = document.querySelector('ion-searchbar');
+  const items = this.users;
+
+  searchbar.addEventListener('ionInput', handleInput); */
+
+  handleInput(event) {
+    const query = event.target.value.toLowerCase();
+    requestAnimationFrame(() => {
+      this.chatsSearch = this.chats.filter((chat)=>{
+        //console.log("q: " +user.username, user.username.indexOf(query));
+        if(chat.name && query != ''){
+          console.log("P ", chat);
+          return (chat.name.toLowerCase().indexOf(query) > -1)
+        }
+        else return chat;
+      });
+    });
+  }
+
 }
