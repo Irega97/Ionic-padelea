781b4cccd1ee95141e177e8ea89ada005dedc1c6
manelfraga <manel.etc.etc@gmail.com>
2021-01-15 21:59:24 +0100

    Atacar a contenedor
    He cambiado la url donde atacamos backend, si quereis probar y no esta funcionando el contenedor, habilitar la linea de localhost y comentar la otra.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import {
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'http://147.83.7.156:3000', options: {} };
 
 @NgModule({
   declarations: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: "http://localhost:3000"
-  //apiURL: "http://147.83.7.155:3000",
+  //apiURL: "http://localhost:3000"
+  apiURL: "http://147.83.7.155:3000",
 };
