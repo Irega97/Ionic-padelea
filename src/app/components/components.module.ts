f6e1851b46532854324912096ae027b478d63adf
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2021-01-26 19:40:24 +0100

    'mapComponent'

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,13 +4,14 @@ import { StatisticCardComponent } from './statistic-card/statistic-card.componen
 import { PuntosCardComponent } from './puntos-card/puntos-card.component';
 import { PublicacionComponent } from './publicacion/publicacion.component';
 import { IonicModule } from '@ionic/angular';
+import { MapaComponent } from './mapa/mapa.component';
 
 @NgModule({
-  declarations: [StatisticCardComponent, PuntosCardComponent, PublicacionComponent],
+  declarations: [StatisticCardComponent, PuntosCardComponent, PublicacionComponent, MapaComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [StatisticCardComponent, PuntosCardComponent, PublicacionComponent]
+  exports: [StatisticCardComponent, PuntosCardComponent, PublicacionComponent, MapaComponent]
 })
 export class ComponentsModule { }
---
+++ src/app/components/mapa/mapa.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Map,tileLayer,marker } from 'leaflet';
+import { MAP_URL } from 'src/environments/config'
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.scss'],
+})
+export class MapaComponent implements OnInit {
+
+  @Input()
+  ubicacion;
+
+  map: Map;
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.loadMap();
+   }
+
+  
+  loadMap(){
+    this.map = new Map('mapId');
+
+    /*const position = await this.locationService.getLocation();
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;*/
+
+    this.map.setView([this.ubicacion.lat, this.ubicacion.lng], 16)
+    tileLayer(MAP_URL, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWlja3lwdXNwYSIsImEiOiJja2s4dnNnc3cwNzEzMnBwYmptcGRlZjVyIn0.gTTzVoYPCbFYJYVh8_Spdg'
+      }).addTo(this.map);
+      let popup = '<b> ' + this.ubicacion.name + '</b><br> Aquí se juega tu torneo'
+    marker([this.ubicacion.lat, this.ubicacion.lng]).addTo(this.map).bindPopup(popup).openPopup();
+
+    console.log("mapa: " , this.map);
+  }
+
+}
